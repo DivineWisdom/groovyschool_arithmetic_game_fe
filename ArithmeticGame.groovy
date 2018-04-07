5a232a83bf98bdf7230c085174b89b2a378b5828
kraulain <fosso@afgrey.com>
2018-04-07 18:35:15 +0100

    Fosso, Debenick, Valdo, Wisdom | add actors to app screen

--- ArithmeticGame.groovy
+++ ArithmeticGame.groovy
@@ -2,3 +2,47 @@
 
 import groovyx.javafx.GroovyFX
 import groovyx.javafx.SceneGraphBuilder
+
+GroovyFX.start {
+def sg = new SceneGraphBuilder()
+def score = 0
+def operation = 'Addition'
+def sign = '+'
+def firstNumber = 0
+def secondNumber = 0
+def feedback = ''
+
+  sg.stage(title: "Groovy School Arithmetic Game", visible: true) {
+    scene(width: 400, height: 500) {
+        vbox(spacing: 10){
+            menuBar(){
+                menu('Operation'){
+                    menuItem('Addition')
+                    menuItem('subtraction')
+                    menuItem('Multiplication')
+                    menuItem('Division')
+                }
+
+            }
+            hbox(spacing: 10, padding:[5,25, 0, 0], alignment: "center_right"){
+                label("Score: ", textFill: black, style: "-fx-font-size: 20pt;")
+                label(score, textFill: black, style: "-fx-font-size: 15pt;")
+            }
+             vbox(spacing: 10, alignment:'center'){
+                label(operation, textFill: black, style: "-fx-font-size: 20pt;")
+                label(firstNumber, textFill: black, style: "-fx-font-size: 30pt;")
+                label(sign, textFill: black, style: "-fx-font-size: 30pt;")
+                label(secondNumber,textFill: black, style: "-fx-font-size: 30pt;")
+                hbox(spacing: 10, alignment:'center'){
+                    label('= ', textFill: black, style: "-fx-font-size: 20pt;")
+                    textField(promptText: 'Answer')
+                }
+                button('Submit', style: "-fx-font-size: 15pt"){
+
+                }
+                label(text: feedback)
+            }
+        }
+      }
+    }
+  }
--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Here is the repository for our first demo GroovyFX project
 
 ## How to run
 
-Make sure GDK and JDK are installed and properly configured.
-Clone the repository.
-Enter the project folder and type
-`groovy ArithmeticGame.groovy`
+..* Make sure GDK and JDK are installed and properly configured.
+..* Clone the repository.
+..* Enter the project folder and type
+```groovy ArithmeticGame.groovy```
