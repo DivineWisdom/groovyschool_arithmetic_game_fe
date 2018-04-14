88992bb40bd6ec73a2320a88d3e51c8825f45096
kraulain <fosso@afgrey.com>
2018-04-14 17:33:57 +0100

    Team | add events handlers for menu items

--- ArithmeticGame.groovy
+++ ArithmeticGame.groovy
@@ -12,28 +12,60 @@ def sign = '+'
 def firstNumber = getRandomInterger()
 def secondNumber = getRandomInterger()
 def feedback = ''
+def operationLable, signLable, firstNumberLable, secondNumberLable
 
   sg.stage(title: "Groovy School Arithmetic Game", visible: true) {
     scene(width: 400, height: 500) {
         vbox(spacing: 10){
             menuBar(){
                 menu('Operation'){
-                    menuItem('Addition')
-                    menuItem('subtraction')
-                    menuItem('Multiplication')
-                    menuItem('Division')
+                    menuItem("Addition", onAction:{ 
+                        operationLable.setText("Addition")
+                        sign = "+"
+                        signLable.setText("+")
+                        firstNumber = getRandomInterger()
+                        firstNumberLable.setText(firstNumber)
+                        secondNumber = getRandomInterger()
+                        secondNumberLable.setText(secondNumber)
+                    })
+                    menuItem("Subtraction", onAction:{ 
+                        operationLable.setText("Subtraction")
+                        sign = "-"
+                        signLable.setText("-")
+                        firstNumber = getRandomInterger()
+                        firstNumberLable.setText(firstNumber)
+                        secondNumber = getRandomInterger()
+                        secondNumberLable.setText(secondNumber)
+                    })
+                    menuItem("Multiplication", onAction:{ 
+                        operationLable.setText("Multiplication")
+                        sign = "X"
+                        signLable.setText("X")
+                        firstNumber = getRandomInterger()
+                        firstNumberLable.setText(firstNumber)
+                        secondNumber = getRandomInterger()
+                        secondNumberLable.setText(secondNumber)
+                    })
+                    menuItem("Division", onAction:{ 
+                        operationLable.setText("Division")
+                        sign = "/"
+                        signLable.setText("/")
+                        firstNumber = getRandomInterger()
+                        firstNumberLable.setText(firstNumber)
+                        secondNumber = getRandomInterger()
+                        secondNumberLable.setText(secondNumber)
+                    })
                 }
-
             }
             hbox(spacing: 10, padding:[5,25, 0, 0], alignment: "center_right"){
                 label("Score: ", textFill: black, style: "-fx-font-size: 20pt;")
                 label(score, textFill: black, style: "-fx-font-size: 15pt;")
             }
              vbox(spacing: 10, alignment:'center'){
-                label(operation, textFill: black, style: "-fx-font-size: 20pt;")
-                label(firstNumber, textFill: black, style: "-fx-font-size: 30pt;")
-                label(sign, textFill: black, style: "-fx-font-size: 30pt;")
-                label(secondNumber,textFill: black, style: "-fx-font-size: 30pt;")
+                operationLable = label(operation, textFill: black, style: "-fx-font-size: 20pt;")
+                firstNumberLable = label(firstNumber, textFill: black, style: "-fx-font-size: 30pt;")
+                signLable = label(sign, textFill: black, style: "-fx-font-size: 30pt;")
+                secondNumberLable = label(secondNumber,textFill: black, style: "-fx-font-size: 30pt;")
                 hbox(spacing: 10, alignment:'center'){
                     label('= ', textFill: black, style: "-fx-font-size: 20pt;")
                     textField(promptText: 'Answer')
@@ -67,4 +99,24 @@ def feedback = ''
 
     def getRandomInterger(){
        Math.abs(new Random().nextInt()%10) + 1
+    }
+
+    def refresh(){
+        firstNumber = getRandomInterger()
+        secondNumber = getRandomInterger()
+    }
+
+    def doSubtraction(){
+        ArithmeticGame.operation = "Subtration"
+        refresh()
+    }
+
+     def doMultiplication(){
+        ArithmeticGame.operation = "Multiplication"
+        refresh()
+    }
+
+    def doDivision(){
+        ArithmeticGame.operation = "Division"
+        refresh()
     }
